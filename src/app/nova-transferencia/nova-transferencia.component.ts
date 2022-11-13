b3e3e15375a96d586abcced9a61d4c94d7f05b9f
Samuel Junior <samuelssj06@gmail.com>
2022-11-13 10:34:33 -0300

    First Commit

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+
+  valor!: number;
+  destino!: number;
+
+transferir(){
+  console.log("TRANSFERIU");
+  console.log("valor: ", this.valor);
+  console.log("destino: ", this.destino);
+
+
+  
+}
+
+
+
+}
